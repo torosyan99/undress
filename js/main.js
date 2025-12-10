5538c36ef926ac55009537ea014bb8805db5fb79
Gfhjkmfl@1@1 <kostechkina@gmail.com>
2025-12-10 11:18:46 +0400

    finish

--- js/main.js
+++ js/main.js
@@ -1,41 +1,44 @@
-let swiper;
-let isDesktop = window.innerWidth >= 768;
+const paymentsSwiper = document.querySelector('.payments__swiper')
 
-function initSwiper() {
-  if (swiper) swiper.destroy(true, true);
+if(paymentsSwiper) {
+  let swiper;
+  let isDesktop = window.innerWidth >= 768;
 
-  swiper = new Swiper('.payments__swiper', {
-    spaceBetween: 16,
-    loop: true,
-    slidesPerView: 1.57,
+  function initSwiper() {
+    if (swiper) swiper.destroy(true, true);
 
-    breakpoints: {
-      768: {
-        slidesPerView: 2.6,
-        direction: 'vertical',
-        centeredSlides: true,
-      },
-      510: {
-        slidesPerView: 2.2,
-      },
-      410: {
-        slidesPerView: 1.8,
+    swiper = new Swiper('.payments__swiper', {
+      spaceBetween: 16,
+      loop: true,
+      slidesPerView: 1.57,
+
+      breakpoints: {
+        768: {
+          slidesPerView: 2.6,
+          direction: 'vertical',
+          centeredSlides: true,
+        },
+        510: {
+          slidesPerView: 2.2,
+        },
+        410: {
+          slidesPerView: 1.8,
+        }
       }
+    });
+  }
+
+  initSwiper();
+
+  window.addEventListener('resize', () => {
+    const nowDesktop = window.innerWidth >= 768;
+
+    if (nowDesktop !== isDesktop) {
+      isDesktop = nowDesktop;
+      initSwiper();
     }
   });
 }
-
-initSwiper();
-
-window.addEventListener('resize', () => {
-  const nowDesktop = window.innerWidth >= 768;
-
-  if (nowDesktop !== isDesktop) {
-    isDesktop = nowDesktop;
-    initSwiper();
-  }
-});
-
 
 let activeFaq = document.querySelector('.faq__item--active');
 
